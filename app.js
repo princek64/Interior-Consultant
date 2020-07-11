18733316bbcacfdae4dbd53b20177d59b9bea733
princek64 <princekukreja64@gmail.com>
2020-07-11 23:23:10 +0530

    Main Section Done

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ const navSlide = () => {
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li')
+const main = document.querySelector('.main')
+const footer = document.querySelector('footer')
+
 
 //Toggle Nav
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
 
 
+
     //Animate Links
     navLinks.forEach((link, index) => {
         if(link.style.animation) {
             link.style.animation = '';
+            main.style.opacity = 1;
+            footer.style.opacity = 1;
         } else {
             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`;
+            main.style.opacity = 0;
+            footer.style.opacity = 0;
         }
     });
 
